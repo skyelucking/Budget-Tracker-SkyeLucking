40af409a05a68347775d753e66171de011113c34
Skye Lucking <lucking.skye@gmail.com>
2021-03-26 00:10:37 -0700

    added some fun elements to the design

--- public/index.js
+++ public/index.js
@@ -72,7 +72,7 @@ function populateChart() {
         datasets: [{
             label: "Total Over Time",
             fill: true,
-            backgroundColor: "#6666ff",
+            backgroundColor: "#FF1616",
             data
         }]
     }
